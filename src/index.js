3de8280c031fa72d8dc1e2f2a4f6e3edb4e41e58
OlegPanadiy <opanadiy@gmail.com>
2023-08-29 11:15:39 +0100

    hw 10

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import { fetchCatByBreed } from './cat-api';
 
 const elements = {
   select: document.querySelector('.breed-select'),
-  loader: document.querySelector('.loader'),
+  loader: document.querySelector('.js-loader'),
   onError: document.querySelector('.error'),
   infoContainer: document.querySelector('.cat-info'),
 };
 
 axios.defaults.headers.common['x-api-key'] =
   'live_4HFK4IlwRBiXmvC6mHPgQ4yAmRdoWRkUmdW5PAEMgVQIrmE1YKLrVz0yhTwB23gc';
-
+elements.select.style.display = 'none';
 fetchBreeds()
   .then(breeds => {
     const breedOpions = breeds
@@ -23,20 +23,30 @@ fetchBreeds()
   })
   .catch(error => {
     throw new Error(Notiflix.Notify.failure(elements.onError.textContent));
+  })
+  .finally(() => {
+    elements.loader.classList.remove('loader'),
+      (elements.select.style.display = 'block');
   });
 
 elements.select.addEventListener('change', handlerOnChange);
 
+elements.loader.classList.add('loader');
+
 function handlerOnChange() {
   const value = elements.select.value;
+  elements.loader.classList.add('loader');
   fetchCatByBreed(value)
     .then(cat => {
       elements.infoContainer.innerHTML = `<img class="cat-img" src="${cat.url}" alt="${cat.breeds[0].name}" width="400" height="300">
 <h2 class="breed=name">${cat.breeds[0].name}</h2>
-<p class="description">Description:${cat.breeds[0].description}</p>
-<p class="breed-temp">Temperament:${cat.breeds[0].temperament}</p>`;
+<p class="description"><span class="text-decotarion">Description:</span> ${cat.breeds[0].description}</p>
+<p class="breed-temp"><span class="text-decotarion">Temperament:</span> ${cat.breeds[0].temperament}</p>`;
     })
     .catch(error => {
       throw new Error(Notiflix.Notify.failure(elements.onError.textContent));
+    })
+    .finally(() => {
+      elements.loader.classList.remove('loader');
     });
 }
